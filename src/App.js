4bde3bfc104dd673934e4150233ffcc246da1959
ihor.nechypor <ihor.nechypor@plej.pl>
2020-07-22 11:45:19 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
+import {Route,Switch } from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component {
   render(){
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path="/auth" component={Quiz}/>
+          <Route path="/quiz-creator" component={Quiz}/>
+          <Route path="/quiz/:id" component={Quiz}/>
+          <Route path="/" component={Quiz}/>
+        </Switch>
       </Layout>  
-    )
+    )ńń
   }
 }
+
 
 
 export default App;
