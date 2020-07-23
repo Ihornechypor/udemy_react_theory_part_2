d977ce1cbe8cec309519fae302b551f85e865a7c
ihor.nechypor <ihor.nechypor@plej.pl>
2020-07-23 10:51:43 +0200

    lesson 75 validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends Component {
     loginHandler = () => {
 
     }
-
 
     registerHandler = () => {
 
@@ -24,8 +24,13 @@ export default class Auth extends Component {
                         Auth
                     </h1>
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <input type="text"/>
-                        <input type="text"/>
+                        <Input
+                            label="email"
+                        />
+                        <Input
+                            label="password"
+                            errorMessage="test"
+                        />
 
                         <Button
                             onClick={this.loginHandler}
