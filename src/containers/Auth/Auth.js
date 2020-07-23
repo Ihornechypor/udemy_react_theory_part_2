8972ba0abf2e2c8edd8e63e8b229b735fd1b5c30
ihor.nechypor <ihor.nechypor@plej.pl>
2020-07-23 09:09:35 +0200

    lesson 74 auth form update

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
+import classes from './Auth.module.css'
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends Component {
+    loginHandler = () => {
+
+    }
+
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <h1>
-                    Auth
-                </h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>
+                        Auth
+                    </h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <Button
+                            onClick={this.loginHandler}
+                            type="success"
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            onClick={this.registerHandler}
+                            type="primary"
+                        >
+                            Register
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
